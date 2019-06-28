108114adfb86aeac6c0fac7e45d5653a86e5ab61
xuziang111 <584788096@qq.com>
2019-06-28 17:35:15 +0800

    调试

--- read/main.js
+++ read/main.js
@@ -7,11 +7,18 @@
 $(document).on('scroll',()=>{
     // let temp = $('main').width()
     console.log('zzz')
-    let temp = $(window).height()-100
-    //窗口可视距离 + 窗口滚动距离 - main元素距离顶部的距离 - book-tool的高度 - book-tool需要离底部的距离
-    $('#book-tool-container').css('top',bookToolPostion())
-    // $(window).scrolltop
-    console.log($(window).height() +  $(window).scrollTop()-$('main').offset().top-50)
-    // console.log($('main').offset().top)
+    if($(window).width()>768){
+        let temp = $(window).height()-100
+        //窗口可视距离 + 窗口滚动距离 - main元素距离顶部的距离 - book-tool的高度 - book-tool需要离底部的距离
+        $('#book-tool-container').css('top',bookToolPostion())
+        // $(window).scrolltop
+        console.log($(window).height() +  $(window).scrollTop()-$('main').offset().top-50)
+        // console.log($('main').offset().top)
+    }
 })
+
+$(window).on('resize',()=>{
+console.log('xxx')
+})
+
 })()
